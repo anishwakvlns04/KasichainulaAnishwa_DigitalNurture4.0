c123ce896406d7781de64d572236cd0e8d85a966
anishwakvlns04 <125719729+anishwakvlns04@users.noreply.github.com>
2025-05-27 11:07:31 +0530

    readd

--- Module3/com/greetings/Main.java
+++
@@ -1,9 +0,0 @@
-package com.greetings;
-
-import com.utils.Utility;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Utility.getGreeting("Simplified Java"));
-    }
-}
